b952c2622f841438fb1ecd67c198aa50d51164b3
wangff <314130948@qq.com>
2018-08-29 21:48:11 +0800

    randomized qucik sort implement

--- sort/quicksort/quicksort.go
+++ sort/quicksort/quicksort.go
@@ -1,5 +1,9 @@
 package quicksort
 
+import (
+	"math/rand"
+)
+
 // QuickSort quick sort
 func QuickSort(A []int, start, end int, isIncrement bool) []int {
 	if start < end {
@@ -7,14 +11,14 @@ func QuickSort(A []int, start, end int, isIncrement bool) []int {
 		QuickSort(A, start, r-1, isIncrement)
 		QuickSort(A, r+1, end, isIncrement)
 	}
+
 	return A
 }
 
 // Partition A with A[end], less than A[end] on the left, more than A[end] on the right
 // When partitioning, A is divided into three blocks, [start, p] is less than A [end], [p + 1, q] is more than A [end], [q, end-1] is waiting for partition
 func partition(A []int, start, end int, isIncrement bool) int {
-	x := end
-	p := start - 1
+	p, x := start-1, end
 	for q := start; q <= end; q++ {
 		if A[x] > A[q] && isIncrement {
 			p++
@@ -25,8 +29,8 @@ func partition(A []int, start, end int, isIncrement bool) int {
 			p++
 			exchange(A, p, q)
 		}
-
 	}
+
 	exchange(A, p+1, x)
 	return p + 1
 }
@@ -35,4 +39,25 @@ func exchange(A []int, x, y int) {
 	temp := A[x]
 	A[x] = A[y]
 	A[y] = temp
+}
+
+// RandomizedQuickSort quick sort for randomized x
+func RandomizedQuickSort(A []int, start, end int, isIncrement bool) []int {
+	if start < end {
+		r := randomizedPartition(A, start, end, isIncrement)
+		RandomizedQuickSort(A, start, r-1, isIncrement)
+		RandomizedQuickSort(A, r+1, end, isIncrement)
+	}
+
+	return A
+}
+
+// RandomizedPartition A with a randomize A[end], less than A[end] on the left, more than A[end] on the right
+// The A[end] is randomly selected by another element exchanged with A[end]
+func randomizedPartition(A []int, start, end int, isIncrement bool) int {
+	// If you set a random number seed for each partition, the performance is too low.
+	// rand.Seed(time.Now().Unix())
+	index := rand.Intn(end-start) + start
+	exchange(A, end, index)
+	return partition(A, start, end, isIncrement)
 }
--- sort/quicksort/quicksort_test.go
+++ sort/quicksort/quicksort_test.go
@@ -3,29 +3,39 @@ package quicksort
 import (
 	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/magiconair/properties/assert"
 )
 
+var (
+	A              = []int{13, 19, 9, 5, 12, 8, 7, 4, 21, 2, 6, 11}
+	ASortIncrement = []int{2, 4, 5, 6, 7, 8, 9, 11, 12, 13, 19, 21}
+	ASortDecrement = []int{21, 19, 13, 12, 11, 9, 8, 7, 6, 5, 4, 2}
+
+	B = []int{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}
+)
+
 func Test_QuickSort(t *testing.T) {
-	A := []int{13, 19, 9, 5, 12, 8, 7, 4, 21, 2, 6, 11}
-	ASortIncrement := []int{2, 4, 5, 6, 7, 8, 9, 11, 12, 13, 19, 21}
 	QuickSort(A, 0, len(A)-1, true)
 	assert.Equal(t, A, ASortIncrement)
 
-	ASortDecrement := []int{21, 19, 13, 12, 11, 9, 8, 7, 6, 5, 4, 2}
 	QuickSort(A, 0, len(A)-1, false)
 	assert.Equal(t, A, ASortDecrement)
 
-	B := []int{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}
 	BSort := B
 	QuickSort(B, 0, len(B)-1, true)
 	assert.Equal(t, B, BSort)
+
+	C := ASortIncrement
+	QuickSort(C, 0, len(C)-1, true)
+	assert.Equal(t, C, ASortIncrement)
 }
 
 func Benchmark_QuickSort(b *testing.B) {
 	size := 1000
 	A := make([]int, size, size)
+	rand.Seed(time.Now().Unix())
 
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
@@ -36,5 +46,36 @@ func Benchmark_QuickSort(b *testing.B) {
 		b.StartTimer()
 		QuickSort(A, 0, len(A)-1, true)
 	}
+}
 
+func Test_RandomizedQuickSort(t *testing.T) {
+	RandomizedQuickSort(A, 0, len(A)-1, true)
+	assert.Equal(t, A, ASortIncrement)
+
+	RandomizedQuickSort(A, 0, len(A)-1, false)
+	assert.Equal(t, A, ASortDecrement)
+
+	BSort := B
+	RandomizedQuickSort(B, 0, len(B)-1, true)
+	assert.Equal(t, B, BSort)
+
+	C := ASortIncrement
+	RandomizedQuickSort(C, 0, len(C)-1, true)
+	assert.Equal(t, C, ASortIncrement)
+}
+
+func Benchmark_RandomizedQuickSort(b *testing.B) {
+	size := 1000
+	A := make([]int, size, size)
+	rand.Seed(time.Now().Unix())
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		for index := 0; index < size; index++ {
+			A[index] = rand.Intn(size)
+		}
+
+		b.StartTimer()
+		RandomizedQuickSort(A, 0, len(A)-1, true)
+	}
 }
